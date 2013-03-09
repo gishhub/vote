597eb758deae0ebb16e760e858f1463cb07ec6ae
Satoshi Ebina <satoshi_2810@hotmail.com>
2013-03-09 18:24:38 +0900

    Third commit.

--- public/js/input.js
+++ public/js/input.js
@@ -1,4 +1,4 @@
-var count = 1;
+var count = 2;
 
 jQuery(document).ready(function($) {
   $("#add_btn").click(function() {
@@ -7,13 +7,58 @@ jQuery(document).ready(function($) {
     $(original).after(cloned);
 
     count++;
+    $(cloned).val("");
     $(cloned).attr("id", "choice"+count);
     $(cloned).attr("name", "choice"+count);
-    $(cloned).attr("value", "");
     $(cloned).attr("placeholder", "Choice"+count);
 
     if (count == 5) {
       $("#add_btn").attr("disabled", true);
     }
+  });
+  
+  $("#question_form").validate({
+    rules: {
+      nickname: {
+        required: true
+      },
+      password: {
+        required: true
+      },
+      title: {
+        required: true
+      },
+      description: {
+        required: true
+      },
+      choice1: {
+        required: true
+      },
+      choice2: {
+        required: true
+      }
+    },
+    messages: {
+      nickname: {
+        required: "ニックネームは入力必須項目です"
+      },
+      password: {
+        required: "パスワードは入力必須項目です"
+      },
+      title: {
+        required: "質問のタイトルは入力必須項目です"
+      },
+      description: {
+        required: "質問の詳細は入力必須項目です"
+      },
+      choice1: {
+        required: "選択肢は最低2つ入力してください"
+      },
+      choice2: {
+        required: "選択肢は最低2つ入力してください"
+      }
+    },
+    errorClass: "alert-error",
+    errorElement: "alert"
   });
 });
